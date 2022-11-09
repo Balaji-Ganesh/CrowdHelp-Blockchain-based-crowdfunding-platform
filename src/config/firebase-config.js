dd2f90c353d36c1978a6b48ebd82c53e22864de6
Balaji-Ganesh <balganeshboda27@gmail.com>
2022-11-09 14:53:04 +0530

    :art: Masked the config details of firebase.

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqkXQ1RXsNc4h1o-crilJ4kyk-kP2sELY",
-  authDomain: "fir-experiments-16493.firebaseapp.com",
-  projectId: "fir-experiments-16493",
-  storageBucket: "fir-experiments-16493.appspot.com",
-  messagingSenderId: "646710968437",
-  appId: "1:646710968437:web:a04fd85f922b759ca0783c",
+  apiKey: process.env.REACT_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.REACT_FIREBASE_APPID,
 };
 
 // Initialize Firebase
