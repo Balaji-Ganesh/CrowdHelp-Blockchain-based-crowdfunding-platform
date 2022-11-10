b84b16f029315e12d286629dae421aacd0ed51d1
Balaji-Ganesh <balganeshboda27@gmail.com>
2022-11-10 17:18:34 +0530

    :bug: #5 Validations fixed for Signin

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -17,6 +17,7 @@ import InputAdornment from "@mui/material/InputAdornment";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import IconButton from "@mui/material/IconButton";
+import FormHelperText from "@mui/material/FormHelperText";
 
 // service imports..
 import firebase from "firebase/compat/app";
@@ -52,6 +53,12 @@ export default function SignIn() {
   /*............................hooks............................*/
   // for validations..
   const [showPassword, setShowPassword] = React.useState(false);
+  const [formValues, setFormValues] = React.useState({
+    email: "",
+    password: "",
+  });
+  const [formErrors, setFormErrors] = React.useState({});
+
   // for showing response...
   const [responseMsg, setResponseMsg] = React.useState(""); // to display error messages.
   const [showResponse, setShowResponse] = React.useState(false); // To know whether error occured. ⁉ why not use length of error message
@@ -61,6 +68,12 @@ export default function SignIn() {
   const navigate = useNavigate(); // for auto-navigation to home page.
 
   /// helpers..
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
   // validations..
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
@@ -69,6 +82,33 @@ export default function SignIn() {
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    console.info("Values received");
+    console.log(values.email);
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (values.email == undefined || values.email == "") {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "Invalid email format!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 6) {
+      errors.password = "Password must be more than 6 characters";
+    } else if (values.password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
+    }
+    console.error(errors);
+    return errors;
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -79,31 +119,37 @@ export default function SignIn() {
     });
 
     // do signin..
-    // perform client-side validations..
-    if (data.get("email") == "" || data.get("password") == "") {
-      setShowResponse(true);
-      setResponseSeverity("error");
-      return setResponseMsg("Fields cannot be empty"); // exit from function..--- 👁️‍🗨️ on this...
+    setFormErrors(validate(formValues)); // perform validation..
+    console.info("Errors received");
+    console.log(formErrors);
+    // Proceed next, only if passed.
+    if (Object.keys(formErrors).length == 0) {
+      console.info("Validation passed");
+      // try {
+      //   // set the messages to default..
+      //   setShowResponse(false);
+      //   setResponseMsg("");
+      //   setIsLoading(true);
+      //   await signInWithEmailAndPassword(
+      //     data.get("email"),
+      //     data.get("password")
+      //   );
+      //   // console.log(data);
+      //   setShowResponse(true);
+      //   setResponseMsg("Sign in success.");
+      //   setResponseSeverity("success");
+      //   // console.log("Sign up success " + showResponse);
+      //   navigate("/"); // auto-navigate to homepage (After successful sign-in)
+      // } catch (error) {
+      //   setShowResponse(true);
+      //   setResponseSeverity("error");
+      //   setResponseMsg(error.message);
+      //   console.log(error);
+      // } finally {
+      //   // after done with sign-in.
+      //   setIsLoading(false);
+      // }
     }
-    try {
-      // set the messages to default..
-      setShowResponse(false);
-      setResponseMsg("");
-      setIsLoading(true);
-      await signInWithEmailAndPassword(data.get("email"), data.get("password"));
-      // console.log(data);
-      setShowResponse(true);
-      setResponseMsg("Sign in success.");
-      setResponseSeverity("success");
-      // console.log("Sign up success " + showResponse);
-      navigate("/"); // auto-navigate to homepage (After successful sign-in)
-    } catch (error) {
-      setShowResponse(true);
-      setResponseSeverity("error");
-      setResponseMsg(error.message);
-      console.log(error);
-    } // after done with sign-in.
-    setIsLoading(false);
   };
 
   const handleClose = (event, reason) => {
@@ -138,32 +184,28 @@ export default function SignIn() {
             sx={{ mt: 1 }}
           >
             <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-              <InputLabel htmlFor="standard-adornment-email">
-                Email Address
-              </InputLabel>
+              <InputLabel>Email Address</InputLabel>
               <Input
-                // id="standard-adornment-email"
                 type="email"
                 name="email"
                 autoComplete="email"
-                // fullWidth
-                // value={"t"}
-                // onChange={handleChange("email")}
+                color={formErrors.email ? "error" : "primary"}
+                onChange={handleChange}
               />
+              {formErrors && (
+                <FormHelperText>{formErrors.email}</FormHelperText>
+              )}
             </FormControl>
 
             <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-              <InputLabel htmlFor="standard-adornment-password">
-                Password
-              </InputLabel>
+              <InputLabel>Password</InputLabel>
               <Input
                 id="standard-adornment-password"
                 type={showPassword ? "text" : "password"}
                 name="password"
                 autoComplete="current-password"
-                fullWidth
-                // value={"t"}
-                // onChange={handleChange("password")}
+                color={formErrors.password ? "error" : "primary"}
+                onChange={handleChange}
                 endAdornment={
                   <InputAdornment position="end">
                     <IconButton
@@ -176,6 +218,9 @@ export default function SignIn() {
                   </InputAdornment>
                 }
               />
+              {formErrors && (
+                <FormHelperText>{formErrors.password}</FormHelperText>
+              )}
             </FormControl>
 
             <Button
