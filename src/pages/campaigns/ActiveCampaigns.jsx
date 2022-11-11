1700ebfa5e9f07d30e4a80d1528cf430a4045e03
Balaji-Ganesh <balganeshboda27@gmail.com>
2022-11-11 22:45:09 +0530

    :zap: #9 Displays campaigns with dummy data

--- src/pages/campaigns/ActiveCampaigns.jsx
+++ src/pages/campaigns/ActiveCampaigns.jsx
@@ -14,6 +14,68 @@ import Footer from "../../components/Footer";
 import NavBar from "../../components/NavBar";
 
 const cards = [1, 2, 3, 4, 5];
+const activeCampaigns = [
+  {
+    bannerUrl:
+      "https://images.unsplash.com/photo-1605289982774-9a6fef564df8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    campaignStatus: "FUNDING IN PROGRESS",
+    isFavoriteCampaign: false,
+    campaignTitle: "Covid vaccinesupply",
+    campaignDescription:
+      "Help us in reaching covid-19 vaccines to reach people out of the city.",
+    ethRaised: "90",
+    ethFunded: "10",
+    daysLeft: "6",
+  },
+  {
+    bannerUrl:
+      "https://images.unsplash.com/photo-1584744982491-665216d95f8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    campaignStatus: "FUNDING IN PROGRESS",
+    isFavoriteCampaign: true,
+    campaignTitle: "Sanitation kits distribution",
+    campaignDescription:
+      "Take part in covid-19 prevention by helping us in distributing sanitation kits to the needy.",
+    ethRaised: "50",
+    ethFunded: "8",
+    daysLeft: "13",
+  },
+  {
+    bannerUrl:
+      "https://images.unsplash.com/photo-1619025873875-59dfdd2bbbd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YW1idWxhbmNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    campaignStatus: "FUNDING IN PROGRESS",
+    isFavoriteCampaign: false,
+    campaignTitle: "Upgrading Emergency vehicles",
+    campaignDescription:
+      "Due to current pandemic, emergency vehicles lacking regular services due to lack of amount, help us to help them.",
+    ethRaised: "80",
+    ethFunded: "59",
+    daysLeft: "5",
+  },
+  {
+    bannerUrl:
+      "https://images.unsplash.com/photo-1513224502586-d1e602410265?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8SUNVfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    campaignStatus: "FUNDING IN PROGRESS",
+    isFavoriteCampaign: true,
+    campaignTitle: "Increasing critical care items",
+    campaignDescription:
+      "Govt. hospitals facing lack of equipment. Please lend a hand to buy more such items.",
+    ethRaised: "120",
+    ethFunded: "98",
+    daysLeft: "6",
+  },
+  {
+    bannerUrl:
+      "https://images.unsplash.com/photo-1607619056574-7b8d3ee536b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=940&q=80",
+    campaignStatus: "FUNDING IN PROGRESS",
+    isFavoriteCampaign: false,
+    campaignTitle: "Medicines supply",
+    campaignDescription:
+      "Due to pandemic, people are facing economical crisis. Help us to save their lives.",
+    ethRaised: "30",
+    ethFunded: "28",
+    daysLeft: "2",
+  },
+];
 
 function ActiveCampaigns() {
   return (
@@ -53,9 +115,9 @@ function ActiveCampaigns() {
         <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <CampaignCard />
+            {activeCampaigns.map((activeCampaign, idx) => (
+              <Grid item key={idx} xs={12} sm={6} md={4}>
+                <CampaignCard details={activeCampaign} />
               </Grid>
             ))}
           </Grid>
